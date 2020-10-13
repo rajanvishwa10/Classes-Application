2ef0e2b8137f897c9ec7e606f610b6b4dd39b1f3
rajanvishwa10 <rajanvishwa10@gmail.com>
2020-10-13 19:07:25 +0530

    Second commit

--- app/src/main/java/com/example/classesapplication/Student/SuggestionsActivity.java
+++ app/src/main/java/com/example/classesapplication/Student/SuggestionsActivity.java
@@ -1,12 +1,20 @@
 package com.example.classesapplication.Student;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.example.classesapplication.R;
 
-public class SuggestionsActivity extends AppCompatActivity {
+public class SuggestionsActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +23,37 @@ public class SuggestionsActivity extends AppCompatActivity {
 
         getWindow().setNavigationBarColor(getResources().getColor(R.color.colorBack));
         getWindow().setStatusBarColor(getResources().getColor(R.color.colorBack));
+
+        Spinner spinner = findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.list, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            super.onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        String text = adapterView.getItemAtPosition(i).toString();
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
 }
